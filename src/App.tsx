bde00cf2734daa70ec98a01070bbaab78ed96864
Plinio <plininhodf@gmail.com>
2022-10-20 22:44:10 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+    <h1 className='font-bold text-5xl text-violet-500'>Hello word</h1>
+
+    <button className='bg-cyan-500 font-medium mt-3 px-4 py-3 rounded text-white hover:bg-cyan-300'>
+      Enviar
+    </button>
+    </>
+  )
+}
+
